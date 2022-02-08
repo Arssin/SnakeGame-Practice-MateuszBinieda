7d60433da61beb9bf5383dfb8a922011460afc6f
Arssin <mateusz.binieda@onet.pl>
2022-02-08 02:06:53 +0100

    Add snake div

--- game.js
+++ game.js
@@ -1,22 +1,29 @@
-import { SNAKE_SPEED } from "./snake";
+import {
+  update as updateSnake,
+  draw as drawSnake,
+  SNAKE_SPEED,
+} from "./snake.js";
 
 let lastRenderTime = 0;
+const gameBoard = document.getElementById("game-board");
 
 function main(currentTime) {
   window.requestAnimationFrame(main);
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
   if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
 
-  console.log("Render");
   lastRenderTime = currentTime;
 
   update();
-
   draw();
 }
 
 window.requestAnimationFrame(main);
 
-function update() {}
+function update() {
+  updateSnake();
+}
 
-function draw() {}
+function draw() {
+  drawSnake(gameBoard);
+}
