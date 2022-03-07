cc3c27bff24da6e555bab18d36cfb77814124644
Arssin <mateusz.binieda@onet.pl>
2022-03-07 21:43:21 +0100

    essa

--- snake.js
+++ snake.js
@@ -10,14 +10,15 @@ const snakeBody = [
   { x: 15, y: 11 },
 ];
 
+//todo ruszyć z tym
 export function update() {
   const inputDirection = getInputDirection();
   for (let i = snakeBody.length - 2; i >= 0; i--) {
     snakeBody[i + 1] = { ...snakeBody[i] };
   }
 
-  snakeBody[0].x += 0;
-  snakeBody[0].y += 1;
+  snakeBody[0].x += inputDirection.x;
+  snakeBody[0].y += inputDirection.y;
 }
 
 export function draw(gameBoard) {
